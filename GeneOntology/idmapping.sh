6b1ec57a78d806baad58340f001e4969600a477e
hojin lee <121307215+hojinlee-98@users.noreply.github.com>
2025-04-17 12:50:02 +0900

    Delete GeneOntology directory

--- GeneOntology/idmapping.sh
+++
@@ -1,6 +0,0 @@
-python ./scripts/20250124_go_term_id_mapping_hj.py ./v2024.1.Hs/GOBP/c5.go.bp.v2024.1.Hs.json ./v2024.1.Hs/GOBP/20250124_c5.go.bp.v2024.1.Hs.term.id_mapping_hj.txt
-python ./scripts/20250124_go_term_id_mapping_hj.py ./v2024.1.Hs/GOCC/c5.go.cc.v2024.1.Hs.json ./v2024.1.Hs/GOCC/20250124_c5.go.cc.v2024.1.Hs.term.id_mapping_hj.txt
-python ./scripts/20250124_go_term_id_mapping_hj.py ./v2024.1.Hs/GOMF/c5.go.mf.v2024.1.Hs.json ./v2024.1.Hs/GOMF/20250124_c5.go.mf.v2024.1.Hs.term.id_mapping_hj.txt
-python ./scripts/20250124_go_term_id_mapping_hj.py ./v2024.1.Mm/GOBP/m5.go.bp.v2024.1.Mm.json ./v2024.1.Mm/GOBP/20250124_m5.go.bp.v2024.1.Mm.term.id_mapping_hj.txt
-python ./scripts/20250124_go_term_id_mapping_hj.py ./v2024.1.Mm/GOCC/m5.go.cc.v2024.1.Mm.json ./v2024.1.Mm/GOCC/20250124_m5.go.cc.v2024.1.Mm.term.id_mapping_hj.txt
-python ./scripts/20250124_go_term_id_mapping_hj.py ./v2024.1.Mm/GOMF/m5.go.mf.v2024.1.Mm.json ./v2024.1.Mm/GOMF/20250124_m5.go.mf.v2024.1.Mm.term.id_mapping_hj.txt
